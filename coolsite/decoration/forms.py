ef2776c8e65764d87fc55c6dd199f6e534a178d3
DefNewUser <def_new_acc@protonmail.com>
2021-11-03 22:21:55 +0300

    Update forms.py register

--- coolsite/decoration/forms.py
+++ coolsite/decoration/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 from .models import *
@@ -23,3 +25,15 @@ class AddPostForm(forms.ModelForm):
             raise ValidationError('Длина превышает 20 символов')
 
         return title
+
+
+class RegisterUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-input'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-input'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-input'}),
+
+        }
--- coolsite/decoration/views.py
+++ coolsite/decoration/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls.base import reverse_lazy
