fbd49c9ae4d573c04a52530375a3396c6434288e
DefNewUser <def_new_acc@protonmail.com>
2021-11-10 23:51:55 +0300

    Update add class ContactFormView

--- coolsite/decoration/forms.py
+++ coolsite/decoration/forms.py
@@ -44,3 +44,4 @@ class LoginUserForm(AuthenticationForm):
     """
     username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
