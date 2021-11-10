a399afb2297164f443340426bf80f9b452bf5334
DefNewUser <def_new_acc@protonmail.com>
2021-11-10 23:55:39 +0300

    Update add form class

--- coolsite/decoration/forms.py
+++ coolsite/decoration/forms.py
@@ -45,3 +45,8 @@ class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Имя', max_length=255)
+    email = forms.EmailField(label='Email')
+    content = forms.CharField(widget=forms.Textarea(attrs={'color': 60, 'rows': 10}))
--- coolsite/decoration/views.py
+++ coolsite/decoration/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls.base import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 
@@ -85,8 +85,24 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
 #     return render(request, 'decoration/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление украшение'})
 
 
-def contact(request):
-    return HttpResponse("Обратная связь")
+# def contact(request):
+#     return HttpResponse("Обратная связь")
+
+
+class ContactFormView(DataMixin, FormView):
+    form_class = ContactForm
+    template_name = 'decoration/contact.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None,  **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Обратная связь")
+        return dict((list(context.items()) + list(c_def.items())))
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return redirect('home')
+
 
 
 # def login(request):
